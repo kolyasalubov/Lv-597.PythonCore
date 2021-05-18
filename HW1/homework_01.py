9d690555eab21ae14d99a81c85ede8e81d003788
Ivan Kulchytskyi <kivan0@gmail.com>
2021-05-19 00:05:13 +0300

    homework 1 added

---
+++ HW1/homework_01.py
@@ -0,0 +1,8 @@
+user_name = input( "What is your name? " )
+user_age = int( input( "How old are you? " ) )
+user_city = input( "Where do you live? " )
+
+print()
+print( "Hello,", user_name )
+print( "Your age is", user_age )
+print( "You live in", user_city )
---
+++ HW1/homework_02.py
@@ -0,0 +1,9 @@
+a_variable = int( input( "give me a=" ) )
+b_variable = int( input( "give me b=" ) )
+
+print()
+print( "a + b =", a_variable + b_variable)
+print( "a - b =", a_variable - b_variable)
+print( "a * b =", a_variable * b_variable)
+print( "a / b =", a_variable / b_variable)
+print( "a * b =", a_variable ** b_variable)
