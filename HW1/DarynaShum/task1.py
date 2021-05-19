81cbd9af477bd0b7e8e34634dde77b4ee37987bb
Daryna Shuminska <shuminskadarunka@gmail.com>
2021-05-19 20:58:20 +0300

    added HW1

---
+++ HW1/DarynaShum/task1.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+print("Hello,", name)
+
+age = input("Enter your age: ")
+print("Your age is", age)
+
+city = input("Enter the name of city you live in: ")
+print("You live in", city)
---
+++ HW1/DarynaShum/task2.py
@@ -0,0 +1,8 @@
+num_a = int(input("Put the number a: "))
+num_b = int(input("Put the number b: ")) 
+
+print('num_a + num_b = ', num_a+num_b)
+print("num_a - num_b =", num_a-num_b)
+print("num_a * num_b = ", num_a*num_b)
+print("num_a / num_b = ", num_a/num_b)
+print("num_a / num_b =", num_a**num_b)
