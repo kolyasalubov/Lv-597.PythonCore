ae192c69e8d9b50a1b3dc81fb14870fa1279b77f
tkovalch <kovalchti@gmail.com>
2021-05-20 00:18:26 +0200

    added Homework1

---
+++ HW1/Kovalchti/1.py
@@ -0,0 +1,9 @@
+#Output questions
+name = input("What is Your name? ")
+age = input("How ols are You? ")
+location = input("Where do You live? ")
+
+# Output information
+print("Hello", name)
+print("Your age is", age)
+print("You live in", location) 
---
+++ HW1/Kovalchti/2.py
@@ -0,0 +1,10 @@
+#Define variables a and b
+a = int(input('Enter the value of a: '))
+b = int(input('Enter the value of b: '))
+
+#Read values a and b from console and do calculations. Output obtained results 
+print('a + b =  ', a + b)
+print('a - b = ', a - b)
+print('a * b = ', a*b)
+print('a / b = ', a/b)
+print('a ** b = ', a**b)
