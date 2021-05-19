48fa9ea0dd1b539c552ed4c059107d6e3919b97d
Vladimir Yezhuninov <vladimir.vy77@gmail.com>
2021-05-19 10:04:26 +0300

    Added folder HW_1&task1.py

---
+++ HW_1/VladimirYezhuninnov/task1.py
@@ -0,0 +1,4 @@
+name = input("What is your name? :")
+age = input("How old are you? :")
+place = input("Where do you live? :")
+print("Hello,{},\n Your age is {},\n You live in {}. " .format(name, age, place))
