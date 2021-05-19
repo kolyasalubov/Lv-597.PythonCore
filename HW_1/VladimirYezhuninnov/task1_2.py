a4205275359402a2517b5c7d436154afabc3dedd
Vladimir Yezhuninov <vladimir.vy77@gmail.com>
2021-05-19 10:10:26 +0300

    Added folder HW_1&task1_2.py

---
+++ HW_1/VladimirYezhuninnov/task1_2.py
@@ -0,0 +1,8 @@
+a = int(input("input a:"))
+b = int(input("input b:"))
+
+print("a + b = ", a + b)
+print("a - b = ", a - b) 
+print("a * b = ", a * b)
+print("a / b = ", a / b)
+print("a**b = ", a ** b)
