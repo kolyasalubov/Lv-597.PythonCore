16f401717540390a726c2279034b9eb16bd19d71
RomanKredo <roman.onishchenko@gmail.com>
2021-05-19 18:24:50 +0300

    first homework

---
+++ main.py
@@ -0,0 +1,21 @@
+# Task 1,  define variables a та b
+a = input("Введіть число а:")
+b = input("Введіть число b:")
+
+# Calculate and output results in numeric format
+print("a+b=", float(a) + float(b))
+print("a-b=", float(a) - float(b))
+print("a*b=", float(a) * float(b))
+print("a/b=", float(a) / float(b))
+print("a**b=", float(a) ** float(b))
+print("Розрахунок завершено")
+
+# Task 2 define variables name, age, place of living
+name = input("What is Your name? ")
+age = input("How aol are You? ")
+live_place = input("Where do You live? ")
+
+# output
+print("Hello", name)
+print("Your age is", age)
+print("You live in", live_place)
