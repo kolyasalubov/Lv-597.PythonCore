1fd286e99566bfc40e271df21c35b660f81dc89e
Ivan Kulchytskyi <kivan0@gmail.com>
2021-05-20 20:16:18 +0300

    Homework 4 Task 2 - type of first element print added

--- HW4/IvanKulchytskyi/task2.py
+++ HW4/IvanKulchytskyi/task2.py
@@ -7,10 +7,10 @@ while list_size > 0:
     list_of_numbers.append( random.randrange( 1, 100 ) )
     list_size -= 1
 
-print( f"original list: {list_of_numbers}; type of [0] element {type( list_of_numbers[0] )}" )
+print( f"original list: {list_of_numbers}\n\t type of first element {type( list_of_numbers[0] )}\n" )
 type( list_of_numbers[0] )
 
 for item in range( len( list_of_numbers ) ):
     list_of_numbers[item] = float( list_of_numbers[item] )
 
-print( f"transformed list: {list_of_numbers}" )
+print( f"transformed list: {list_of_numbers}\n\t type of first element {type( list_of_numbers[0] )}" )
