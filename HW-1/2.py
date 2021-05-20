b9a367763bfa8d759abdf6e664be03367008f720
Denys Soltys <pemp56931@gmail.com>
2021-05-20 12:22:40 +0200

    HW commit

---
+++ HW-1/2.py
@@ -0,0 +1,9 @@
+name=input("What is your name?")
+age=input("How ald are you?")
+place=input("Where do you live?")
+            
+print("Hello",name)
+print("Your age is",age)
+print("You live in",place)
+      
+            
