9f7dfa020b408aab39448f1609d3b7aee4e96e5c
tkovalch <kovalchti@gmail.com>
2021-05-20 02:00:24 +0200

    added Homework3

---
+++ HW3/Kovalchti/2.py
@@ -0,0 +1,18 @@
+#Come up with a four digit number 
+
+a = '1234'
+
+#Multiplication
+
+m_a = int(a[0]) * int(a[1]) * int(a[2]) * int(a[3])
+print(m_a)
+
+#Reverse order
+
+backwards_list = (a[::-1]) 
+print(''.join(backwards_list))
+
+#Sorting
+
+print(sorted(a)) 
+
---
+++ HW3/Kovalchti/3.py
@@ -0,0 +1,8 @@
+#Change 2 variables without engaging the 3rd one
+
+a = 'x'
+b = 'y'
+
+a, b = b, a
+
+print(a,b) 
