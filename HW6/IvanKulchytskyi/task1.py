a692ff4df78efefc1c66433ae6bdbc9b631ed1fe
Ivan Kulchytskyi <kivan0@gmail.com>
2021-05-28 00:03:54 +0300

    Task 1 DocString added

--- HW6/IvanKulchytskyi/task1.py
+++ HW6/IvanKulchytskyi/task1.py
@@ -1,11 +1,29 @@
 def largest_max(number_one, number_two):
+    """ This function compares two integer numbers 
+    and returns largest number using max function
+    Args:
+        number_one (int): The first number.
+        number_two (int): The second number.
+
+    Returns:
+        int: Largest number.
+    """
     return max(number_one, number_two)
 
 def largest_if(number_one, number_two):
+    """ This function compares two integer numbers 
+    and returns largest using сomparision оperator
+    Args:
+        number_one (int): The first number.
+        number_two (int): The second number.
+
+    Returns:
+        int: Largest number.
+    """
     return number_one if number_one > number_two else number_two
 
 
-first_num = int(input("Enter fistt number:"))
+first_num = int(input("Enter first number:"))
 second_num = int(input("Enter second number:"))
 
 print(f"largest of {first_num} and {second_num} is {largest_max(first_num, second_num)}, {largest_if(first_num, second_num)} ")
