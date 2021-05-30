d8608a317e9a9094d880d639623cdd4a85485dd9
RomanKredo <roman.onishchenko@gmail.com>
2021-05-30 20:47:26 +0300

    Hw6_tasks_update

--- HW6/RomanKredo/3.py
+++ HW6/RomanKredo/3.py
@@ -1,7 +1,14 @@
 # Write a function that calculates the number of characters included in a given string
 def check_freq(count_string):
+    """
+    Function calculate count of letters in string
+    :param count_string: string for calculating
+    :return: dict with results
+    """
     freqncy = {}
     for c in count_string.lower():
         freqncy[c] = count_string.count(c)
     return freqncy
+
+
 print(check_freq("Write a function that calculates the number of characters included in a given string"))
