ad1495507913d9c170105fcbde2680c72af7ae75
EmmaSamoilova <emmasamojlova@gamil.com>
2021-06-05 14:10:37 +0300

    added correct solution

--- HW6/EmmaSamoilova/t_3.py
+++ HW6/EmmaSamoilova/t_3.py
@@ -1,6 +1,14 @@
-def count_symbol1(str):
-    return len(str)
+#def count_symbol1(str):
+#    return len(str)
 
-def count_symbol2(string):
-    return sum(map(lambda x: len(x), string.split()))
+#def count_symbol2(string):
+#    return sum(map(lambda x: len(x), string.split()))
     
+def count_symbol(string):
+    symbols = {}
+    for symbol in string:
+        if symbol in symbols:
+            continue
+        else:
+            symbols[symbol] = string.count(symbol)
+    return symbols
