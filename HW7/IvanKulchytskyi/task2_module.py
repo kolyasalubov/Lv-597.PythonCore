ef2bee4a8b519815a3228b4da910aa40fd5704b8
Ivan Kulchytskyi <kivan0@gmail.com>
2021-06-08 19:56:33 +0300

    HW7 - task2 m odule new functions with re

--- HW7/IvanKulchytskyi/task2_module.py
+++ HW7/IvanKulchytskyi/task2_module.py
@@ -1,3 +1,4 @@
+import re
 from string import (
     digits,
     ascii_lowercase,
@@ -27,3 +28,25 @@ def is_to_short(string):
 
 def is_to_long(string):
     return False if maximum_length == 0 else len(string) > maximum_length
+
+
+def re_has_symbol(symbols, string):#
+    return any(string.count(symbol) for symbol in symbols) if symbols else False
+
+def re_has_digit(string):#
+    return re_has_symbol(digits, string)
+
+def re_has_upper(string):#
+    return re_has_symbol(ascii_uppercase, string)
+
+def re_has_lower(string):#
+    return re_has_symbol(ascii_lowercase, string)
+
+def re_has_special_characters(string):#
+    return re_has_symbol(special_characters, string)
+
+def re_is_to_short(string):
+    return False if minimal_length == 0 else not re.fullmatch(".{" + str(minimal_length) + ",}", string)
+
+def re_is_to_long(string):
+    return False if maximum_length == 0 else not re.fullmatch(".{0," + str(maximum_length) + "}", string)
