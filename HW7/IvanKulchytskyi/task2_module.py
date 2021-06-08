6c569c44bdf1f714d4d835e39b7f380fc00c2b6e
Ivan Kulchytskyi <kivan0@gmail.com>
2021-06-08 20:14:59 +0300

    HW7 - task2 module re functions

--- HW7/IvanKulchytskyi/task2_module.py
+++ HW7/IvanKulchytskyi/task2_module.py
@@ -30,20 +30,20 @@ def is_to_long(string):
     return False if maximum_length == 0 else len(string) > maximum_length
 
 
-def re_has_symbol(symbols, string):#
-    return any(string.count(symbol) for symbol in symbols) if symbols else False
+def re_has_symbol(symbols, string):
+    return re.search(symbols, string)
 
-def re_has_digit(string):#
-    return re_has_symbol(digits, string)
+def re_has_digit(string):
+    return re_has_symbol(r"\d", string)
 
-def re_has_upper(string):#
-    return re_has_symbol(ascii_uppercase, string)
+def re_has_upper(string):
+    return re_has_symbol(r"[A-Z]", string)
 
-def re_has_lower(string):#
-    return re_has_symbol(ascii_lowercase, string)
+def re_has_lower(string):
+    return re_has_symbol(r"[a-z]", string)
 
-def re_has_special_characters(string):#
-    return re_has_symbol(special_characters, string)
+def re_has_special_characters(string):
+    return re_has_symbol(r"[" + special_characters + "]", string)
 
 def re_is_to_short(string):
     return False if minimal_length == 0 else not re.fullmatch(".{" + str(minimal_length) + ",}", string)
