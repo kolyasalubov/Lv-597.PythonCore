f64eb8c800cf466ffcb342b6c3334f95433c5c8c
VladGranat <84147412+VladGranat@users.noreply.github.com>
2021-06-09 23:24:01 +0300

    CWhw8

---
+++ CodeWars/VladGranat/8.py
@@ -0,0 +1,10 @@
+def count_sheeps(sheep):
+    print (sheep.count(True))
+
+sheep = [True,  True,  True,  False,
+True,  True,  True,  True ,True,  False,
+ True,  False,True,  False, False, True ,
+ True,  True,  True,  True ,False, False, 
+ True,  True]
+
+count_sheeps(sheep)
