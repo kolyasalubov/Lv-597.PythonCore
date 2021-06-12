7b11da909b9c073b169cadbef2f01b8297256f6b
VladGranat <84147412+VladGranat@users.noreply.github.com>
2021-06-12 23:04:14 +0300

    CWhw10

---
+++ CodeWars/VladGranat/10.py
@@ -0,0 +1,11 @@
+def distance(x1, y1, x2, y2):
+    
+    C = (((x2-x1)**2) + ((y2-y1)**2)) **0.5
+    
+    print (round(C,2))
+
+distance(0,0,1,1)
+
+# def distance(x1, y1, x2, y2):
+#     C = (((x2-x1)**2)+((y2-y1)**2))**0.5
+#     return round(C,2)
