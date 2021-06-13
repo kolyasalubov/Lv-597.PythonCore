af97c96de0e1f2306f37e3539417402c7117c61b
VladGranat <84147412+VladGranat@users.noreply.github.com>
2021-06-13 18:55:58 +0300

    CWhw12

---
+++ CodeWars/VladGranat/12.py
@@ -0,0 +1,17 @@
+def create_array(n):
+    r =[]
+    i=0
+    while i<n: 
+        i+=1
+        r+=[i]
+    print (r)
+
+create_array(10)
+
+# def create_array(n):
+#     r =[]
+#     i=0
+#     while i<n: 
+#         i+=1
+#         r+=[i]
+#     return r
