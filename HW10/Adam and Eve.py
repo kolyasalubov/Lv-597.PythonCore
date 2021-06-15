7ffbfbae014cd133a9f5e53a8276ce9eee13cdbb
tkovalch <kovalchti@gmail.com>
2021-06-15 13:36:20 +0200

    added Homework10

---
+++ HW10/Adam and Eve.py
@@ -0,0 +1,14 @@
+#Basic subclasses - Adam and Eve
+
+class Human:
+    pass
+
+class Man(Human):
+    pass
+
+class Woman(Human):
+    pass
+
+def God():
+    return [Man(), Woman()]
+
---
+++ HW10/Color Ghost.py
@@ -0,0 +1,7 @@
+#Color Ghost 
+
+import random
+class Ghost(object):
+  def __init__(self):
+    self.color = random.choice(["white", "yellow", "purple", "red"])
+
