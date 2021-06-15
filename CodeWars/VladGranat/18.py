e365de6dd6a365403f1f469c7acc172226788181
VladGranat <84147412+VladGranat@users.noreply.github.com>
2021-06-16 00:12:16 +0300

    CWhw18

---
+++ CodeWars/VladGranat/18.py
@@ -0,0 +1,16 @@
+def double_char(s):
+    r = ''
+    for i in s:
+        r += i * 2
+        
+    print (r)
+
+
+s = ('Hello')
+double_char(s)
+
+# def double_char(s):
+#     r = ''
+#     for i in s:
+#         r += i * 2
+#     return r
