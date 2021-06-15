55f22296caf2ab4940779378bdcd0e58ed04309d
VladGranat <84147412+VladGranat@users.noreply.github.com>
2021-06-15 22:21:48 +0300

    CWhw17

---
+++ CodeWars/VladGranat/17.py
@@ -0,0 +1,14 @@
+def list_animals(animals):
+     list = ''
+     for i in range(len(animals)):
+        list += str(i + 1) + '. ' + animals[i] + '\n'
+        print (list)
+
+animals = ['dog', 'cat', 'elephant']
+list_animals(animals)
+
+# def list_animals(animals):
+#     list = ''
+#     for i in range(len(animals)):
+#         list += str(i + 1) + '. ' + animals[i] + '\n'
+#     return list
