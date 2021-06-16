f3e0e275bc2935a2aec48cc1317504406e3463b4
“Oleksandr <sanfork1776@gmail.com>
2021-06-16 23:44:18 +0300

    updated some HWs

--- HW9/OleksandrForkun/slidfe81emplo00p.py
+++ HW9/OleksandrForkun/slidfe81emplo00p.py
@@ -8,12 +8,19 @@
 
 class Employee:
     """Doc string for employees. thank you."""
+
+    counter_employee = 0
+
     def __init__(self, name, salary):
         self.name = name
         self.salary = int(salary)
+        Employee.counter_employee += 1
 
     def empl_info(self):
         return f'Name: {self.name} - Salary: {self.salary}$.'
+
+    def empl_count(self):
+        print(Employee.counter_employee)
 
 # print(Employee.__base__)
 # print(Employee.__dict__)
@@ -21,6 +28,10 @@ class Employee:
 # print(Employee.__module__)
 # print(Employee.__doc__)
 
+# test
+
 bob1 =  Employee('Bob', 1200)
+rob2 = Employee('Rob', 1500)
 
 print(bob1.empl_info())
+bob1.empl_count()
