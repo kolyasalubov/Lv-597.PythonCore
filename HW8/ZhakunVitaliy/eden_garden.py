0b02479965ccfc3a5fa6df06ee9a6eebd45ec56a
Vitaliy Zhakun <vizarce@gmail.com>
2021-06-16 15:47:52 +0300

    added solutions

---
+++ HW8/ZhakunVitaliy/eden_garden.py
@@ -0,0 +1,14 @@
+class Human:
+    pass
+
+class Man(Human):
+    pass
+
+class Woman(Human):
+    pass
+
+def God():
+    eden_garden = []
+    eden_garden.append(Man())
+    eden_garden.append(Woman())
+    return eden_garden
