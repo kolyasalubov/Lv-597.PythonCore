42f445c30592c960b8eebc07517a7230d79e7e9c
Nazar <55602048+nazariymakogin@users.noreply.github.com>
2021-06-17 15:19:59 +0300

    Update classy_classes.py

--- HW9/classy_classes.py
+++ HW9/classy_classes.py
@@ -1,9 +1,18 @@
 class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-        self._info = f'{self.name}s age is {self.age}'
+    def __init__(self, name,age):
+        self.__name = name
+        self.__age = age
+        self.info = self.info()
+
+    
+    @property
+    def name(self):
+        return self.__name
 
     @property
+    def age(self):
+        return self.__age
+
+
     def info(self):
-        return self._info
+        return f"{str(self.name)}s age is {int(self.age)}"
